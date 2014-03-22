3f154abf4745951af69d93731ce11d86871680f5
donghaomin <dhmcharles@gmail.com>
2014-03-22 17:28:44 +0100

    [2/2] User configurable clear all recents button
    * Add a button on recent panel to clear all recent apps,
        and an option to alter its position(or switch off).
    * Add new drawables for clear all recents button,
        instead of using ic_notify_clear from notification area.
        Thanks @arzbhatia
    
    [2/2] 用户可调整的清除全部最近应用按钮
    
    * 在"最近打开的应用"面板中添加一个"清除全部最近应用"按钮,
        以及一个选项来调整它的位置(或禁用它).
    * 添加"清除全部最近应用按钮"专用的drawables,
        而不是沿用通知面板下拉里的ic_notify_clear.
        感谢 @arzbhatia
    
    [1/2] in platform_frameworks_base

--- src/com/android/settings/biantai/SystemUiSettings.java
+++ src/com/android/settings/biantai/SystemUiSettings.java
@@ -41,7 +41,10 @@
 package com.android.settings.biantai;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -49,16 +52,36 @@ import com.android.settings.SettingsPreferenceFragment;
 public class SystemUiSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "SystemUiSettings";
+    // Clear all recents button
+    private static final String CLEAR_ALL_RECENTS_BUTTON = "clear_all_recents_button";
+
+    private ListPreference mClearAllRecentsButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.system_ui_settings);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        // Clear all recents button position/switch
+        mClearAllRecentsButton = (ListPreference) findPreference(CLEAR_ALL_RECENTS_BUTTON);
+        int clearStatus = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.CLEAR_ALL_RECENTS_BUTTON, 4);
+        mClearAllRecentsButton.setValue(String.valueOf(clearStatus));
+        mClearAllRecentsButton.setSummary(mClearAllRecentsButton.getEntry());
+        mClearAllRecentsButton.setOnPreferenceChangeListener(this);
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mClearAllRecentsButton) {
+            int value = Integer.valueOf((String) objValue);
+            int index = mClearAllRecentsButton.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.CLEAR_ALL_RECENTS_BUTTON, value);
+            mClearAllRecentsButton.setSummary(mClearAllRecentsButton.getEntries()[index]);
+            return true;
+        }
         return false;
     }
 }
